7ff3560865cf7fd9237490754214285d99947122
JoshWilbur <joshuawilbur03@gmail.com>
2023-12-16 20:50:40 -0500

    solved the git crisis

---
+++ display.cpp
@@ -0,0 +1,86 @@
+// Compile with: g++ -o display display.cpp display_func.cpp input.c ADA1893.c SGP30.c -lbcm2835 -lrt -lST7735_TFT_RPI
+// Run using: sudo ./display
+
+#include <bcm2835.h>
+#include <iostream>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "display.hpp"
+extern "C" {
+	#include "input.c"
+	#include "ADA1893.c"
+	#include "SGP30.c"
+}
+
+
+int main(void) {
+	input_setup();
+	if(!Setup())return -1;
+	ada1893_setup(0);
+	sgp30Init();
+	// Create buffers and variables for readings
+	char pres[30], temp[30], gas[30], alt[30], p_min[30], t_min[30], g_min[30], a_min[30], p_max[30], t_max[30], g_max[30], a_max[30];
+	float temp_read, pres_read, gas_read, alt_read, temp_max, pres_max, gas_max, alt_max = 0;
+	float temp_min = 999;
+	float pres_min = 999;
+	float gas_min = 999;
+	float alt_min = 999;
+
+	// Infinite loop to allow for continuous use
+	while(1){
+		// Run the functions defined in the other programs
+		ada1893_setup(0);
+		temp_read = read_temp();
+		std::sprintf(temp, "Temperature: %.2fC", temp_read); // Fill in buffers with text to be displayed
+
+                alt_read = read_alt();
+                std::sprintf(alt, "Altitude: %.2f M", alt_read);
+
+		ada1893_setup(1);
+		delay(50);
+	        pres_read = read_pres();
+	        std::sprintf(pres, "Pressure: %.2fkPa", pres_read);
+
+		gas_read = sgp30Measure();
+                std::sprintf(gas, "Co2: %.2f ppm", gas_read);
+
+		// Due to having to switch between the barometer and altmeter, readings may be wrong at times, ignore unrealistic values
+		// Collect maximum readings
+		if (temp_read > temp_max) temp_max = temp_read;
+		if (pres_read > pres_max) pres_max = pres_read;
+		if (alt_read > alt_max && alt_read < 20000) alt_max = alt_read;
+		if (gas_read > gas_max) gas_max = gas_read;
+
+                // Collect minimum readings
+                if (temp_read < temp_min) temp_min = temp_read;
+                if (pres_read < pres_min && pres_read > 10) pres_min = pres_read;
+                if (alt_read < alt_min) alt_min = alt_read;
+                if (gas_read < gas_min && gas_min > 10) gas_min = gas_read;
+
+		// Create strings to display
+	 	std::sprintf(t_min, "Min Temp: %.2f C", temp_min);
+		std::sprintf(t_max, "Max Temp: %.2f C", temp_max);
+                std::sprintf(p_min, "Min Pres: %.2fkPa", pres_min);
+                std::sprintf(p_max, "Max Pres: %.2fkPa", pres_max);
+
+                std::sprintf(a_min, "Min Alt: %.2f M", alt_min);
+                std::sprintf(a_max, "Max Alt: %.2f M", alt_max);
+                std::sprintf(g_min, "Min Co2: %.2f ppm", gas_min);
+                std::sprintf(g_max, "Max Co2: %.2f ppm", gas_max);
+
+		// Change screen if button is pressed, cycles through
+		if (read_input() == 1){
+			while(1){
+				Screen_Two(alt, gas, a_min, a_max, g_min, g_max);
+				delay(1000);
+				if (read_input() == 1) break;
+			}
+		}
+		Screen_One(temp, pres, t_min, t_max, p_min, p_max);
+		delay(50);
+	}
+	Shutdown_Display();
+	return 0;
+}
---
+++ display_func.cpp
@@ -0,0 +1,102 @@
+#include <bcm2835.h>
+#include <iostream>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <thread>
+#include <chrono>
+
+#include "ST7735_TFT_RPI-1.6/include/ST7735_TFT_graphics.hpp"
+#include "ST7735_TFT_RPI-1.6/include/ST7735_TFT.hpp"
+#include "ST7735_TFT_RPI-1.6/include/ST7735_TFT_Print.hpp"
+
+// This function will handle function declarations for the "display.hpp" header file
+
+// Global struct myTFT
+ST7735_TFT myTFT;
+
+
+// Static declaration of functions
+int8_t Setup(void);
+void Test_Text(void);
+void Screen_One(char* temp, char* pressure, char* temp_min, char* temp_max, char* pres_min, char* pres_max);
+void Screen_Two(char* alt, char* gas, char* alt_min, char* alt_max, char* gas_min, char* gas_max);
+void Shutdown_Display(void);
+
+// This function will create the layout for displaying temperature and humidity
+void Screen_One(char* temp, char* pressure, char* temp_min, char* temp_max, char* pres_min, char* pres_max){
+	myTFT.TFTfillScreen(ST7735_BLACK);
+	myTFT.TFTsetRotation(myTFT.TFT_Degrees_270); // Rotate screen so its in landscape mode
+	myTFT.TFTfillRect(0, 62, 160, 4, ST7735_GREEN); // Green horizontal line
+	myTFT.TFTFontNum(myTFT.TFTFont_Tiny);
+	myTFT.TFTdrawText(4, 5, temp, ST7735_WHITE, ST7735_BLACK, 2); // Display temperature
+        myTFT.TFTdrawText(4, 25, temp_min, ST7735_WHITE, ST7735_BLACK, 2); // Display min temperature
+        myTFT.TFTdrawText(4, 45, temp_max, ST7735_WHITE, ST7735_BLACK, 2); // Display max temperature
+        myTFT.TFTdrawText(4, 75, pressure, ST7735_WHITE, ST7735_BLACK, 2); // Display humidity
+        myTFT.TFTdrawText(4, 95, pres_min, ST7735_WHITE, ST7735_BLACK, 2); // Display min humidity
+        myTFT.TFTdrawText(4, 110, pres_max, ST7735_WHITE, ST7735_BLACK, 2); // Display max humidity
+}
+
+// This function will create the layout for displaying altitude and gas
+void Screen_Two(char* alt, char* gas, char* alt_min, char* alt_max, char* gas_min, char* gas_max){
+        myTFT.TFTfillScreen(ST7735_BLACK);
+        myTFT.TFTsetRotation(myTFT.TFT_Degrees_270); // Rotate screen so its in landscape mode
+        myTFT.TFTfillRect(0, 62, 160, 4, ST7735_CYAN); // Green horizontal line
+        myTFT.TFTFontNum(myTFT.TFTFont_Tiny);
+        myTFT.TFTdrawText(4, 5, alt, ST7735_WHITE, ST7735_BLACK, 2); // Display altitude
+        myTFT.TFTdrawText(4, 25, alt_min, ST7735_WHITE, ST7735_BLACK, 2); // Display min altitude
+        myTFT.TFTdrawText(4, 45, alt_max, ST7735_WHITE, ST7735_BLACK, 2); // Display max altitude
+        myTFT.TFTdrawText(4, 75, gas, ST7735_WHITE, ST7735_BLACK, 2); // Display gas reading
+        myTFT.TFTdrawText(4, 95, gas_min, ST7735_WHITE, ST7735_BLACK, 2); // Display min gas
+        myTFT.TFTdrawText(4, 110, gas_max, ST7735_WHITE, ST7735_BLACK, 2); // Display max gas
+}
+
+// Setup function from TFT library
+int8_t Setup(void)
+{
+	std::cout << "TFT Start" << std::endl;
+	if(!bcm2835_init())
+	{
+		std::cout << "Error : Problem with init bcm2835 library" << std::endl;
+		return -1;
+	}
+
+// GPIO/SPI TYPE
+	int8_t RST_TFT = 25;
+	int8_t DC_TFT = 24;
+	int8_t SCLK_TFT = -1;   // 5, change to any GPIO no for sw spi, -1 hw spi
+	int8_t SDIN_TFT = -1;   // 6, change to any GPIO no for sw spi, -1 hw spi
+	int8_t CS_TFT = -1;  // 8, change to any GPIO no for sw spi, -1 hw spi
+	myTFT.TFTSetupGPIO(RST_TFT, DC_TFT, CS_TFT, SCLK_TFT, SDIN_TFT);
+
+
+// Screen Setup
+	uint8_t OFFSET_COL = 0;  // 2, These offsets can be adjusted for any issues->
+	uint8_t OFFSET_ROW = 0; // 3, with manufacture tolerance/defects
+	uint16_t TFT_WIDTH = 128; // Screen width
+	uint16_t TFT_HEIGHT = 160; // Screen height
+	myTFT.TFTInitScreenSize(OFFSET_COL, OFFSET_ROW , TFT_WIDTH , TFT_HEIGHT);
+
+// PCB_TYPE + SPI baud rate + SPI_CE_PIN
+	uint32_t SCLK_FREQ =  8000000 ; // HW Spi only , freq in Hertz , MAX 125 Mhz MIN 30Khz
+	uint8_t SPI_CE_PIN = 0; // HW Spi only which HW SPI chip enable pin to use,  0 or 1
+	if(!myTFT.TFTInitPCBType(myTFT.TFT_ST7735R_Red, SCLK_FREQ, SPI_CE_PIN))return -1;
+	return 0;
+}
+
+void Test_Text(void) {
+	char teststr1[] = "testing"; // String to hold what is printed
+	myTFT.TFTfillScreen(ST7735_BLACK);
+	myTFT.TFTFontNum(myTFT.TFTFont_Default);
+        myTFT.TFTfillScreen(ST7735_BLACK);
+        myTFT.TFTsetRotation(myTFT.TFT_Degrees_90); // Rotate screen so its in landscape mode
+	myTFT.TFTdrawText(15, 30, teststr1, ST7735_WHITE, ST7735_BLACK, 1);
+	std::this_thread::sleep_for(std::chrono::seconds(3)); // Display for three seconds before ending function
+}
+
+void Shutdown_Display(void)
+{
+	myTFT.TFTPowerDown(); // Power down device
+	bcm2835_close(); // Close the bcm2835 library
+	std::cout << "TFT End" << std::endl;
+}
